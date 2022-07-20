6df13edf7a05b0144a9bd216576aaf39d1a73bb8
Nkusi Joe <joenkusi01@gmail..com>
2022-07-20 19:16:19 +0200

    User

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+const {getAllUsers, createUser,updateOneUser, getOneUser, deleteOneUser} = require('../controllers/user')
+
+const routes = express.Router()
+
+routes.route('/:_id').get(getOneUser).delete(deleteOneUser).put.apply(updateOneUser)
+routes.route('/').get(getAllUsers).post(createUser)
+
+module.exports = routes
